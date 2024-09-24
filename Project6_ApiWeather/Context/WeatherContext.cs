f0d6213b33c95081c072c6cfb619caaba5bfbe2b
kaansarii <skaan45145@gmail.com>
2024-09-24 22:26:26 +0300

    ####6.proje tamamlandi####

---
+++ Project6_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+
+
+    // Entity Framework Core ile bir veritabanı bağlamı (context) oluşturan bir sınıftır.
+    // WeatherContext sınıfı, veritabanı işlemlerini yönetmek için kullanılır ve veritabanıyla etkileşime geçmek isteyen sınıfların bir yapı taşıdır. 
+    public class WeatherContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= Kaan\\SQLEXPRESS;initial catalog= Db6Project20; integrated Security=true; TrustServerCertificate=True");
+        }
+        public DbSet<City> Cities { get; set; }
+    }
+}
