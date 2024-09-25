30a33c57ef6ce57a3fb7f5823c3d8d7e46253764
kaansarii <skaan45145@gmail.com>
2024-09-25 23:33:36 +0300

    ####8.proje tamamlandi.####

---
+++ Project8_RapidApiCurrency/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Project8_RapidApiCurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.txtUniPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblSterlin = new System.Windows.Forms.Label();
+            this.rdbDolar = new System.Windows.Forms.RadioButton();
+            this.rdbEuro = new System.Windows.Forms.RadioButton();
+            this.rdbSterlin = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Location = new System.Drawing.Point(34, 14);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(37, 29);
+            this.lblDolar.TabIndex = 0;
+            this.lblDolar.Text = "---";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Location = new System.Drawing.Point(32, 14);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(37, 29);
+            this.lblEuro.TabIndex = 2;
+            this.lblEuro.Text = "---";
+            // 
+            // txtUniPrice
+            // 
+            this.txtUniPrice.Location = new System.Drawing.Point(281, 137);
+            this.txtUniPrice.Name = "txtUniPrice";
+            this.txtUniPrice.Size = new System.Drawing.Size(260, 34);
+            this.txtUniPrice.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Birim Tutar:";
+            // 
+            // btnAction
+            // 
+            this.btnAction.BackColor = System.Drawing.Color.Coral;
+            this.btnAction.Location = new System.Drawing.Point(206, 252);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(185, 36);
+            this.btnAction.TabIndex = 9;
+            this.btnAction.Text = "İşlemi Yap";
+            this.btnAction.UseVisualStyleBackColor = false;
+            this.btnAction.Click += new System.EventHandler(this.buttonAction_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ödenecek Tutar:";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(281, 195);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(260, 34);
+            this.txtTotalPrice.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Chartreuse;
+            this.panel1.Controls.Add(this.lblDolar);
+            this.panel1.Location = new System.Drawing.Point(33, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(161, 58);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gold;
+            this.panel2.Controls.Add(this.lblEuro);
+            this.panel2.Location = new System.Drawing.Point(227, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(164, 58);
+            this.panel2.TabIndex = 13;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.panel3.Controls.Add(this.lblSterlin);
+            this.panel3.Location = new System.Drawing.Point(419, 9);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(164, 58);
+            this.panel3.TabIndex = 14;
+            // 
+            // lblSterlin
+            // 
+            this.lblSterlin.AutoSize = true;
+            this.lblSterlin.Location = new System.Drawing.Point(35, 14);
+            this.lblSterlin.Name = "lblSterlin";
+            this.lblSterlin.Size = new System.Drawing.Size(37, 29);
+            this.lblSterlin.TabIndex = 2;
+            this.lblSterlin.Text = "---";
+            // 
+            // rdbDolar
+            // 
+            this.rdbDolar.AutoSize = true;
+            this.rdbDolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbDolar.Location = new System.Drawing.Point(72, 84);
+            this.rdbDolar.Name = "rdbDolar";
+            this.rdbDolar.Size = new System.Drawing.Size(74, 26);
+            this.rdbDolar.TabIndex = 15;
+            this.rdbDolar.TabStop = true;
+            this.rdbDolar.Text = "Dolar";
+            this.rdbDolar.UseVisualStyleBackColor = true;
+            // 
+            // rdbEuro
+            // 
+            this.rdbEuro.AutoSize = true;
+            this.rdbEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbEuro.Location = new System.Drawing.Point(264, 84);
+            this.rdbEuro.Name = "rdbEuro";
+            this.rdbEuro.Size = new System.Drawing.Size(69, 26);
+            this.rdbEuro.TabIndex = 16;
+            this.rdbEuro.TabStop = true;
+            this.rdbEuro.Text = "Euro";
+            this.rdbEuro.UseVisualStyleBackColor = true;
+            // 
+            // rdbSterlin
+            // 
+            this.rdbSterlin.AutoSize = true;
+            this.rdbSterlin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbSterlin.Location = new System.Drawing.Point(459, 84);
+            this.rdbSterlin.Name = "rdbSterlin";
+            this.rdbSterlin.Size = new System.Drawing.Size(82, 26);
+            this.rdbSterlin.TabIndex = 17;
+            this.rdbSterlin.TabStop = true;
+            this.rdbSterlin.Text = "Sterlin";
+            this.rdbSterlin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(602, 300);
+            this.Controls.Add(this.rdbSterlin);
+            this.Controls.Add(this.rdbEuro);
+            this.Controls.Add(this.rdbDolar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUniPrice);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.TextBox txtUniPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblSterlin;
+        private System.Windows.Forms.RadioButton rdbDolar;
+        private System.Windows.Forms.RadioButton rdbEuro;
+        private System.Windows.Forms.RadioButton rdbSterlin;
+    }
+}
+
