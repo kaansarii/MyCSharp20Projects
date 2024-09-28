842d393edbdc8ac170bc81868c05a2af1c0e502c
kaansarii <skaan45145@gmail.com>
2024-09-28 23:38:42 +0300

    ####15.proje tamamlandi.####

---
+++ Project15_GasPriceSimulation/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Project15_GasPriceSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtGasAmount = new System.Windows.Forms.TextBox();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtLPGPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDieselPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGasolinePrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rdbGasoline = new System.Windows.Forms.RadioButton();
+            this.rdbDiesel = new System.Windows.Forms.RadioButton();
+            this.rdbLPG = new System.Windows.Forms.RadioButton();
+            this.txtStation = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yakıt Miktarı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 183);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Toplam Tutar: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 243);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dolum Durumu:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(163, 235);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(418, 33);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // txtGasAmount
+            // 
+            this.txtGasAmount.Location = new System.Drawing.Point(163, 123);
+            this.txtGasAmount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtGasAmount.Name = "txtGasAmount";
+            this.txtGasAmount.Size = new System.Drawing.Size(231, 30);
+            this.txtGasAmount.TabIndex = 4;
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(163, 178);
+            this.txtTotalPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(231, 30);
+            this.txtTotalPrice.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(293, 291);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(169, 42);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "İşlemi Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txtLPGPrice);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtDieselPrice);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtGasolinePrice);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(660, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 193);
+            this.panel1.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 13);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 25);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Fiyatlar";
+            // 
+            // txtLPGPrice
+            // 
+            this.txtLPGPrice.BackColor = System.Drawing.Color.Gold;
+            this.txtLPGPrice.Location = new System.Drawing.Point(103, 148);
+            this.txtLPGPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLPGPrice.Name = "txtLPGPrice";
+            this.txtLPGPrice.ReadOnly = true;
+            this.txtLPGPrice.Size = new System.Drawing.Size(85, 30);
+            this.txtLPGPrice.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 151);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "LPG:";
+            // 
+            // txtDieselPrice
+            // 
+            this.txtDieselPrice.BackColor = System.Drawing.Color.Gold;
+            this.txtDieselPrice.Location = new System.Drawing.Point(103, 95);
+            this.txtDieselPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDieselPrice.Name = "txtDieselPrice";
+            this.txtDieselPrice.ReadOnly = true;
+            this.txtDieselPrice.Size = new System.Drawing.Size(85, 30);
+            this.txtDieselPrice.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 98);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Motorin:";
+            // 
+            // txtGasolinePrice
+            // 
+            this.txtGasolinePrice.BackColor = System.Drawing.Color.Gold;
+            this.txtGasolinePrice.Location = new System.Drawing.Point(103, 43);
+            this.txtGasolinePrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtGasolinePrice.Name = "txtGasolinePrice";
+            this.txtGasolinePrice.ReadOnly = true;
+            this.txtGasolinePrice.Size = new System.Drawing.Size(85, 30);
+            this.txtGasolinePrice.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 46);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Benzin:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Stencil", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(277, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(342, 39);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "YAKIT ALIM SİSTEMİ";
+            // 
+            // rdbGasoline
+            // 
+            this.rdbGasoline.AutoSize = true;
+            this.rdbGasoline.Location = new System.Drawing.Point(12, 75);
+            this.rdbGasoline.Name = "rdbGasoline";
+            this.rdbGasoline.Size = new System.Drawing.Size(93, 29);
+            this.rdbGasoline.TabIndex = 9;
+            this.rdbGasoline.TabStop = true;
+            this.rdbGasoline.Text = "Benzin";
+            this.rdbGasoline.UseVisualStyleBackColor = true;
+            // 
+            // rdbDiesel
+            // 
+            this.rdbDiesel.AutoSize = true;
+            this.rdbDiesel.Location = new System.Drawing.Point(202, 75);
+            this.rdbDiesel.Name = "rdbDiesel";
+            this.rdbDiesel.Size = new System.Drawing.Size(98, 29);
+            this.rdbDiesel.TabIndex = 10;
+            this.rdbDiesel.TabStop = true;
+            this.rdbDiesel.Text = "Motorin";
+            this.rdbDiesel.UseVisualStyleBackColor = true;
+            // 
+            // rdbLPG
+            // 
+            this.rdbLPG.AutoSize = true;
+            this.rdbLPG.Location = new System.Drawing.Point(395, 75);
+            this.rdbLPG.Name = "rdbLPG";
+            this.rdbLPG.Size = new System.Drawing.Size(72, 29);
+            this.rdbLPG.TabIndex = 11;
+            this.rdbLPG.TabStop = true;
+            this.rdbLPG.Text = "LPG";
+            this.rdbLPG.UseVisualStyleBackColor = true;
+            // 
+            // txtStation
+            // 
+            this.txtStation.Location = new System.Drawing.Point(650, 67);
+            this.txtStation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtStation.Name = "txtStation";
+            this.txtStation.ReadOnly = true;
+            this.txtStation.Size = new System.Drawing.Size(231, 30);
+            this.txtStation.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(929, 359);
+            this.Controls.Add(this.txtStation);
+            this.Controls.Add(this.rdbLPG);
+            this.Controls.Add(this.rdbDiesel);
+            this.Controls.Add(this.rdbGasoline);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.txtGasAmount);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtGasAmount;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLPGPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDieselPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGasolinePrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton rdbGasoline;
+        private System.Windows.Forms.RadioButton rdbDiesel;
+        private System.Windows.Forms.RadioButton rdbLPG;
+        private System.Windows.Forms.TextBox txtStation;
+    }
+}
+
---
+++ Project15_GasPriceSimulation/Form1.cs
@@ -0,0 +1,156 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project15_GasPriceSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double dieselPrice = 0;
+        double gasolinePrice = 0;
+        double lpgPrice = 0;
+
+        double gasAmout = 0;
+        double totalPrice = 0;
+        int count = 0;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            gasAmout = Convert.ToDouble(txtGasAmount.Text);
+            timer1.Start();
+            timer1.Interval = 500;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = count.ToString();
+            if (rdbGasoline.Checked)
+            { 
+                count++;
+                if(count <= gasAmout)
+                {
+                    totalPrice += gasolinePrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    progressBar1.Value  += 3;
+                    if (progressBar1.Value == count)
+                    {
+                        timer1.Stop();
+                    }
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    timer1.Stop();
+
+                }
+               
+            }
+
+            if (rdbDiesel.Checked)
+            {
+                count++;
+                if (count <= gasAmout)
+                {
+                    totalPrice += dieselPrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    progressBar1.Value += 3;
+                    if (progressBar1.Value == count)
+                    {
+                        timer1.Stop();
+                    }
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    timer1.Stop();
+
+                }
+
+            }
+
+
+            if (rdbLPG.Checked)
+            {
+                count++;
+                if (count <= gasAmout)
+                {
+                    totalPrice += lpgPrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    progressBar1.Value += 3;
+                    if (progressBar1.Value == count)
+                    {
+                        timer1.Stop();
+                    }
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                    timer1.Stop();
+
+                }
+
+            }
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+    {
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://akaryakit-fiyatlari.p.rapidapi.com/fuel/istanbul"),
+            Headers =
+        {
+            { "x-rapidapi-key", "2746dba02fmsh977643c5a626a1ap18d1abjsn279e636d6159" },
+            { "x-rapidapi-host", "akaryakit-fiyatlari.p.rapidapi.com" },
+        },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+
+            var json = JObject.Parse(body);
+            var gasolineJsonValue = json["data"][16]["prices"][0]["benzin"];
+            var dieselJsonValue = json["data"][16]["prices"][0]["motorin"];
+            var lpgJsonValue = json["data"][16]["prices"][0]["lpg"];
+            var stationName = json["data"][16]["prices"][0]["dagitici_firma"];
+
+            // Kültürel ayarlarla birlikte string'i double'a çevir
+            var cultureInfo = new CultureInfo("tr-TR");
+
+            if (double.TryParse(gasolineJsonValue.ToString(), NumberStyles.Any, cultureInfo, out double gasolineParsed))
+            {
+                gasolinePrice = gasolineParsed;
+            }
+            if (double.TryParse(dieselJsonValue.ToString(), NumberStyles.Any, cultureInfo, out double dieselParsed))
+            {
+                dieselPrice = dieselParsed;
+            }
+            if (double.TryParse(lpgJsonValue.ToString(), NumberStyles.Any, cultureInfo, out double lpgParsed))
+            {
+                lpgPrice = lpgParsed;
+            }
+
+            txtStation.Text = stationName.ToString();
+            txtGasolinePrice.Text = gasolinePrice.ToString("F2", cultureInfo);  // Formatlı yazdırma
+            txtDieselPrice.Text = dieselPrice.ToString("F2", cultureInfo);
+            txtLPGPrice.Text = lpgPrice.ToString("F2", cultureInfo);
+        }
+    }
+
+}
+}
