a966c8f8065524d30460bd0cfcbe79a5a438f2ea
kaansarii <skaan45145@gmail.com>
2024-09-28 15:26:51 +0300

    ####13.proje tamamlandi.####

---
+++ Project_13WeatherApp/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Project_13WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCelcius = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblRuzgar = new System.Windows.Forms.Label();
+            this.lblNem = new System.Windows.Forms.Label();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KIRŞEHİR, TR";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 181);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "KIRŞEHİR, TR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Bugün";
+            // 
+            // lblCelcius
+            // 
+            this.lblCelcius.AutoSize = true;
+            this.lblCelcius.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCelcius.Location = new System.Drawing.Point(209, 181);
+            this.lblCelcius.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCelcius.Name = "lblCelcius";
+            this.lblCelcius.Size = new System.Drawing.Size(146, 58);
+            this.lblCelcius.TabIndex = 4;
+            this.lblCelcius.Text = "00.00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(331, 161);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "C";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(215, 239);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 24);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Bugün";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Aqua;
+            this.label7.Location = new System.Drawing.Point(17, 317);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Rüzgar:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Aqua;
+            this.label8.Location = new System.Drawing.Point(17, 364);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 24);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Nem:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.Aqua;
+            this.label9.Location = new System.Drawing.Point(17, 404);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 30);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Fahrenheit:";
+            // 
+            // lblRuzgar
+            // 
+            this.lblRuzgar.AutoSize = true;
+            this.lblRuzgar.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRuzgar.ForeColor = System.Drawing.Color.Aqua;
+            this.lblRuzgar.Location = new System.Drawing.Point(125, 317);
+            this.lblRuzgar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRuzgar.Name = "lblRuzgar";
+            this.lblRuzgar.Size = new System.Drawing.Size(32, 24);
+            this.lblRuzgar.TabIndex = 10;
+            this.lblRuzgar.Text = "00";
+            // 
+            // lblNem
+            // 
+            this.lblNem.AutoSize = true;
+            this.lblNem.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNem.ForeColor = System.Drawing.Color.Aqua;
+            this.lblNem.Location = new System.Drawing.Point(125, 358);
+            this.lblNem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNem.Name = "lblNem";
+            this.lblNem.Size = new System.Drawing.Size(32, 24);
+            this.lblNem.TabIndex = 11;
+            this.lblNem.Text = "00";
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFahrenheit.ForeColor = System.Drawing.Color.Aqua;
+            this.lblFahrenheit.Location = new System.Drawing.Point(125, 404);
+            this.lblFahrenheit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(32, 24);
+            this.lblFahrenheit.TabIndex = 12;
+            this.lblFahrenheit.Text = "00";
+           //this.lblFahrenheit.Click += new System.EventHandler(this.lblFahrenheit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(363, 452);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.lblNem);
+            this.Controls.Add(this.lblRuzgar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblCelcius);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.SlateGray;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCelcius;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblRuzgar;
+        private System.Windows.Forms.Label lblNem;
+        private System.Windows.Forms.Label lblFahrenheit;
+    }
+}
+
---
+++ Project_13WeatherApp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_13WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city/landon/EN"),
+                Headers =
+    {
+        { "x-rapidapi-key", "2746dba02fmsh977643c5a626a1ap18d1abjsn279e636d6159" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+
+                var fahrenheit = json["main"]["feels_like"].ToString();
+                var ruzgar = json["wind"]["speed"].ToString();
+                var nem = json["main"]["humidity"].ToString();
+
+                lblFahrenheit.Text = fahrenheit;
+                lblRuzgar.Text = ruzgar;
+                lblNem.Text = nem;
+
+                double celcius = (double.Parse(fahrenheit) - 32);
+                double celciusValue = celcius / 1.8;
+                lblCelcius.Text = celciusValue.ToString("0.00");
+
+
+
+
+                var weatherCondition = json["weather"][0]["main"].ToString();
+                // Switch-case bloğu: hava durumuna göre resim değiştirme
+                switch (weatherCondition.ToLower())
+                {
+                    case "clear":
+                        pictureBox1.Image = Image.FromFile(@"C:\Users\Kaan Sari\Desktop\Udemy 20 Proje\MyUdemy20Projects\Project_13WeatherApp\Images\sun.png");
+
+                        break;
+
+                    case "clouds":
+                        pictureBox1.Image = Image.FromFile(@"Images\cloud.png");
+                        break;
+
+                    case "rain":
+                        pictureBox1.Image = Image.FromFile(@"Images\rain.png");
+                        break;
+
+                    case "snow":
+                        pictureBox1.Image = Image.FromFile(@"Images\snow.png");
+                        break;
+
+                    case "thunderstorm":
+                        pictureBox1.Image = Image.FromFile(@"Images\thunderstorm.png");
+                        break;
+
+                    case "drizzle":
+                        pictureBox1.Image = Image.FromFile(@"Images\rain.png");
+                        break;
+
+                    case "mist":
+                    case "fog":
+                        pictureBox1.Image = Image.FromFile(@"Images\mist.png");
+                        break;
+
+                    default:
+                        pictureBox1.Image = Image.FromFile(@"Images\black.png");
+                        break;
+                }
+            }
+        }
+    }
+}
